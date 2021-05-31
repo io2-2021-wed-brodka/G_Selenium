779c39169eace6f6e178c1c7fcab8425fb3028ee
Tomek Karwowski <to.karwowski@gmail.com>
2021-05-31 22:34:42 +0200

    refactor tests as unittests

--- main.py
+++ main.py
@@ -3,14 +3,15 @@ from time import sleep
 
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.ui import WebDriverWait
 
 SHOW_BROWSER = True  # TODO(tkarwowski): move to env
-ADMIN_ENDPOINT = "http://127.0.0.1:8080"
-USER_TECH_ENDPOINT = "http://127.0.0.1:8081"
+ADMIN_ENDPOINT = "http://127.0.0.1:3000"
+USER_TECH_ENDPOINT = "http://127.0.0.1:3001"
 
 
-class SeleniumAdminTest(unittest.TestCase):
+class SeleniumUserTechTest(unittest.TestCase):
     browser = None
 
     @classmethod
@@ -24,6 +25,7 @@ class SeleniumAdminTest(unittest.TestCase):
             executable_path="./geckodriver", options=firefox_options
         )
         cls.browser.delete_all_cookies()
+        cls.random_login = "easy_login"
 
     @classmethod
     def tearDownClass(cls):
@@ -39,28 +41,123 @@ class SeleniumAdminTest(unittest.TestCase):
         if SHOW_BROWSER:
             sleep(seconds)
 
-    def test_login(self):
-        self.wait_a_little()
-        self.browser.get(f"{ADMIN_ENDPOINT}/login")
-        # wait untill page shows up
+    def test_register(self):
+        self.browser.get(f"{USER_TECH_ENDPOINT}/login")
+        self.wait_a_little(2)
+        elem = self.browser.find_element_by_id(id_="sign-up-container")
+        elem.click()
+        self.wait_a_little(2)
+        elem = self.browser.find_element_by_id(id_="register-login")
+        elem.send_keys(self.random_login)
+        self.wait_a_little(2)
+        elem = self.browser.find_element_by_id(id_="register-password")
+        elem.send_keys("newPassword")
+        self.wait_a_little(1)
+        elem = self.browser.find_element_by_id(id_="register-button-confirm")
+        elem.click()
+        self.wait_a_little(5)
 
-        # insert login
+    def testLoginUserPage(self):
+        self.browser.get(f"{USER_TECH_ENDPOINT}/login")
+        self.wait_a_little(2)
+        elem = self.browser.find_element_by_id(id_="login-login")
+        elem.send_keys(self.random_login)
+        self.wait_a_little(2)
+        elem = self.browser.find_element_by_id(id_="login-password")
+        elem.send_keys("newPassword")
+        self.wait_a_little(2)
+        elem = self.browser.find_element_by_id(id_="login-button-confirm")
+        elem.click()
+        self.wait_a_little(5)
 
-        # insert password
+    def testLogoutUserPage(self):
+        self.browser.get(f"{USER_TECH_ENDPOINT}/login")
+        elem = self.browser.find_element_by_id(id_="login-login")
+        elem.send_keys(self.random_login)
+        elem = self.browser.find_element_by_id(id_="login-password")
+        elem.send_keys("newPassword")
+        elem = self.browser.find_element_by_id(id_="login-button-confirm")
+        elem.click()
+        self.wait_a_little(2)
+        elem = self.browser.find_element_by_id(id_="logout-button-confirm")
+        elem.click()
+        self.wait_a_little(5)
 
-        # press LOG IN button
+    def testViewStationListUser(self):
+        self.browser.get(f"{USER_TECH_ENDPOINT}/login")
+        elem = self.browser.find_element_by_id(id_="login-login")
+        elem.send_keys(self.random_login)
+        elem = self.browser.find_element_by_id(id_="login-password")
+        elem.send_keys("newPassword")
+        elem = self.browser.find_element_by_id(id_="login-button-confirm")
+        elem.click()
+        elem = self.browser.find_element_by_id(id_="topbar-station-button-confirm")
+        elem.click()
+        self.wait_a_little(5)
 
-        # asset redirects to
-        self.wait_a_little()
-        self.wait_until_element_found("//span[.='LOGIN']")
-        self.wait_a_little()
-        element = self.browser.find_element_by_xpath("//span[.='LOGIN']")
-        self.wait_a_little()
+    def testViewBikeListUser(self):
+        self.browser.get(f"{USER_TECH_ENDPOINT}/login")
+        elem = self.browser.find_element_by_id(id_="login-login")
+        elem.send_keys(self.random_login)
+        elem = self.browser.find_element_by_id(id_="login-password")
+        elem.send_keys("newPassword")
+        elem = self.browser.find_element_by_id(id_="login-button-confirm")
+        elem.click()
+        elem = self.browser.find_element_by_id(id_="topbar-station-button-confirm")
+        elem.click()
+        self.wait_a_little(2)
+        elem = self.browser.find_element_by_id(id_="station-button-confirm-0")
+        elem.click()
+        self.wait_a_little(5)
 
-    # test case function to check the Person.get_name function
-    def test_1_get_name(self):
-        print("\nStart get_name test\n")
-        print("\nFinish get_name test\n")
+    def testRentBiketUser(self):
+        elem = self.browser.find_element_by_id(id_="login-login")
+        elem.send_keys(self.random_login)
+        elem = self.browser.find_element_by_id(id_="login-password")
+        elem.send_keys("newPassword")
+        elem = self.browser.find_element_by_id(id_="login-button-confirm")
+        elem.click()
+        elem = self.browser.find_element_by_id(id_="topbar-station-button-confirm")
+        elem.click()
+        elem = self.browser.find_element_by_id(id_="station-button-confirm-2")
+        elem.click()
+        self.wait_a_little(1)
+        elem = self.browser.find_element_by_id(id_="bike-rent-button-confirm-0")
+        elem.click()
+        self.wait_a_little(1)
+        elem = self.browser.find_element_by_id(id_="bike-rent-button-confirm")
+        elem.click()
+        self.wait_a_little(5)
+
+    def testReturnBiketUser(self):
+        self.browser.get(f"{USER_TECH_ENDPOINT}/login")
+        elem = self.browser.find_element_by_id(id_="login-login")
+        elem.send_keys(self.random_login)
+        elem = self.browser.find_element_by_id(id_="login-password")
+        elem.send_keys("newPassword")
+        elem = self.browser.find_element_by_id(id_="login-button-confirm")
+        elem.click()
+        elem = self.browser.find_element_by_id(id_="topbar-station-button-confirm")
+        elem.click()
+        elem = self.browser.find_element_by_id(id_="station-button-confirm-2")
+        elem.click()
+        elem = self.browser.find_element_by_id(id_="bike-rent-button-confirm-0")
+        elem.click()
+        elem = self.browser.find_element_by_id(id_="bike-rent-button-confirm")
+        elem.click()
+        self.wait_a_little(1)
+        elem = self.browser.find_element_by_id(id_="topbar-rented-button-confirm")
+        elem.click()
+        self.wait_a_little(1)
+        elem = self.browser.find_element_by_id(id_="return-bike-button-0")
+        elem.click()
+        self.wait_a_little(1)
+        stationListSelect = Select(self.browser.find_element_by_id(id_="select-return-bike-station"))
+        stationListSelect.select_by_visible_text("Uniwersytet")
+        self.wait_a_little(1)
+        elem = self.browser.find_element_by_id(id_="dialog-return-bike-button-confirm")
+        elem.click()
+        self.wait_a_little(5)
 
 
 if __name__ == '__main__':
--- run_tests.sh
+++ run_tests.sh
@@ -4,7 +4,7 @@
 echo "Instantiating backend..."
 cd ../G_Backend || return
 source venv/bin/activate
-python manage.py runserver_plus --cert certname > /dev/null 2> /dev/null &
+python manage.py runserver_plus 127.0.0.1:8080 --cert certname > /dev/null 2> /dev/null &
 
 # run admin
 echo "Instantiating admin frontend..."
@@ -14,6 +14,9 @@ npm run start > /dev/null &
 echo "Instantiating user-tech backend..."
 cd ../G_User-Tech || return
 npm run start > /dev/null &
+
+echo "Wait for pages to start up"
+sleep 5
 
 echo "Testing..."
 cd ../G_Selenium || return
